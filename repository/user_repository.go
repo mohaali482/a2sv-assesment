c47e1553b3143dcb160a4318f3a3701b08b0fdbc
Mohammed Ali <mohaali482@gmail.com>
2024-08-28 11:00:26 +0300

    feat(repository): add insert function of user repository

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/mohaali482/a2sv-assesment/domain"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserRepository interface {
@@ -13,4 +15,46 @@ type UserRepository interface {
 	Insert(ctx context.Context, user *domain.User) error
 	Update(ctx context.Context, user *domain.User) error
 	Delete(ctx context.Context, id string) error
+}
+
+const userCollection = "users"
+
+type UserRepositoryImpl struct {
+	database *mongo.Database
+}
+
+func NewUserRepository(database *mongo.Database) UserRepository {
+	return &UserRepositoryImpl{database: database}
+}
+
+// Delete implements UserRepository.
+func (u *UserRepositoryImpl) Delete(ctx context.Context, id string) error {
+	panic("unimplemented")
+}
+
+// FindByEmail implements UserRepository.
+func (u *UserRepositoryImpl) FindByEmail(ctx context.Context, email string) (*domain.User, error) {
+	panic("unimplemented")
+}
+
+// FindByID implements UserRepository.
+func (u *UserRepositoryImpl) FindByID(ctx context.Context, id string) (*domain.User, error) {
+	panic("unimplemented")
+}
+
+// GetUsers implements UserRepository.
+func (u *UserRepositoryImpl) GetUsers(ctx context.Context) ([]*domain.User, error) {
+	panic("unimplemented")
+}
+
+// Insert implements UserRepository.
+func (u *UserRepositoryImpl) Insert(ctx context.Context, user *domain.User) error {
+	user.ID = primitive.NewObjectID().Hex()
+	_, err := u.database.Collection(userCollection).InsertOne(ctx, user)
+	return err
+}
+
+// Update implements UserRepository.
+func (u *UserRepositoryImpl) Update(ctx context.Context, user *domain.User) error {
+	panic("unimplemented")
 }
