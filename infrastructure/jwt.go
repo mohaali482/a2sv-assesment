7f2b6f34301b703bd38bad540c412957ade7d3b8
Mohammed Ali <mohaali482@gmail.com>
2024-08-28 11:41:28 +0300

    feat(infrastructure): add jwt implementation

---
+++ infrastructure/jwt.go
@@ -0,0 +1,66 @@
+package infrastructure
+
+import (
+	"github.com/golang-jwt/jwt"
+	"github.com/mohaali482/a2sv-assesment/domain"
+)
+
+type JWTService interface {
+	GenerateToken(user *domain.User) (*domain.Token, error)
+	ValidateToken(token string) (map[string]interface{}, error)
+}
+
+type JWTServiceImpl struct {
+	secretKey string
+}
+
+func NewJWTService(secretKey string) JWTService {
+	return &JWTServiceImpl{secretKey: secretKey}
+}
+
+// GenerateToken implements JWTService.
+func (j *JWTServiceImpl) GenerateToken(user *domain.User) (*domain.Token, error) {
+	accessTokenclaims := jwt.MapClaims{
+		"id":    user.ID,
+		"email": user.Email,
+		"role":  user.Role,
+		"type":  "access",
+	}
+
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, accessTokenclaims)
+	accessTokenString, err := accessToken.SignedString([]byte(j.secretKey))
+	if err != nil {
+		return nil, err
+	}
+
+	refreshTokenclaims := jwt.MapClaims{
+		"id":    user.ID,
+		"email": user.Email,
+		"role":  user.Role,
+		"type":  "refresh",
+	}
+
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshTokenclaims)
+	refreshTokenString, err := refreshToken.SignedString([]byte(j.secretKey))
+	if err != nil {
+		return nil, err
+	}
+
+	return &domain.Token{
+		Access:  accessTokenString,
+		Refresh: refreshTokenString,
+	}, nil
+}
+
+// ValidateToken implements JWTService.
+func (j *JWTServiceImpl) ValidateToken(token string) (map[string]interface{}, error) {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(j.secretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return claims, nil
+}
